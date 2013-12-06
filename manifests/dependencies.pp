474651c992daa2445854b95285c597c421fec0f5
Wharenn <wharenn@gmail.com>
2013-12-06 12:16:30 +0100

    Add dependencies for redhat based systems and check if defined

--- manifests/dependencies.pp
+++ manifests/dependencies.pp
@@ -2,20 +2,89 @@
 class redmine::dependencies  {
 
   exec { 'gem-rails':
-    command => 'gem install rails':
+    command => 'gem install rails --no-rdoc --no-ri',
     creates => '/usr/bin/rails',
   }
 
   exec { 'gem-bundler':
-    command => 'gem install bundler':
+    command => 'gem install bundler',
     creates => '/usr/bin/bundle',
     require => Exec['gem-rails'],
   }
 
+  if !defined(Package["wget"]) {
+    package { 'wget':
+      ensure  => present,
+    }
+  }
+
+  if !defined(Package["tar"]) {
+    package { 'tar':
+      ensure  => present,
+    }
+  }
+
+  if !defined(Package["make"]) {
+    package { 'make':
+      ensure  => present,
+    }
+  }
+
+  if !defined(Package["gcc"]) {
+    package { 'gcc':
+      ensure  => present,
+    }
+  }
+
   if $::osfamily == 'Debian' {
-    package {['libmysql++-dev','libmysqlclient-dev','libmagickcore-dev','libmagickwand-dev']:
-      ensure => installed,
-      before => Exec['bundle_redmine'],
+    if !defined(Package["libmysql++-dev"]) {
+      package { 'libmysql++-dev':
+        ensure => present,
+        before => Exec['bundle_redmine'],
+      }
+    }
+    if !defined(Package["libmysqlclient-dev"]) {
+      package { 'libmysqlclient-dev':
+        ensure => present,
+        before => Exec['bundle_redmine'],
+      }
+    }
+    if !defined(Package["libmagickcore-dev"]) {
+      package { 'libmagickcore-dev':
+        ensure => present,
+        before => Exec['bundle_redmine'],
+      }
+    }
+    if !defined(Package["libmagickwand-dev"]) {
+      package { 'libmagickwand-dev':
+        ensure => present,
+        before => Exec['bundle_redmine'],
+      }
+    }
+  } elsif $::osfamily == 'Redhat' {
+    if !defined(Package["mysql-devel"]) {
+      package { 'mysql-devel':
+        ensure => present,
+        before => Exec['bundle_redmine'],
+      }
+    }
+    if !defined(Package["postgresql-devel"]) {
+      package { 'postgresql-devel':
+        ensure => present,
+        before => Exec['bundle_redmine'],
+      }
+    }
+    if !defined(Package["sqlite-devel"]) {
+      package { 'sqlite-devel':
+        ensure => present,
+        before => Exec['bundle_redmine'],
+      }
+    }
+    if !defined(Package["ImageMagick-devel"]) {
+      package { 'ImageMagick-devel':
+        ensure => present,
+        before => Exec['bundle_redmine'],
+      }
     }
   }
 }
