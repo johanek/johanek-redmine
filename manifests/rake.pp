bf76ef4e98428f8c811ba0acb5d01f6dd549e005
Alexander Buddenbrock <a.buddenbrock@ish.de>
2014-08-24 11:04:11 +0200

    Removed hardcoded touch paths

--- manifests/rake.pp
+++ manifests/rake.pp
@@ -10,20 +10,20 @@ class redmine::rake {
 
   # Create session store
   exec { 'session_store':
-    command => 'rake generate_session_store && /bin/touch .session_store',
+    command => 'rake generate_session_store && touch .session_store',
     creates => "${redmine::webroot}/.session_store"
   }
 
   # Perform rails migrations
   exec { 'rails_migrations':
-    command => 'rake db:migrate && /bin/touch .migrate',
+    command => 'rake db:migrate && touch .migrate',
     creates => "${redmine::webroot}/.migrate",
     notify  => Class['apache::service']
   }
 
   # Seed DB data
   exec { 'seed_db':
-    command => 'rake redmine:load_default_data && /bin/touch .seed',
+    command => 'rake redmine:load_default_data && touch .seed',
     creates => "${redmine::webroot}/.seed",
     notify  => Class['apache::service'],
     require => Exec['rails_migrations']
