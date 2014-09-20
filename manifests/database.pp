838b92c3284fc9b67cba0a7688651c5335eeeab8
Alexander Buddenbrock <a.buddenbrock@ish.de>
2014-09-20 19:25:24 +0200

    Fixed mysql grant validation error
    The latest puppetlabs/mysql version from git complains that the name and
    the user/table names do not match. The problem beeing the missing ".*"
    at the end.

--- manifests/database.pp
+++ manifests/database.pp
@@ -16,13 +16,13 @@ class redmine::database {
       password_hash => mysql_password($redmine::database_password)
     }
 
-    mysql_grant { "${redmine::database_user}@${redmine::database_server}/${redmine::production_database}":
+    mysql_grant { "${redmine::database_user}@${redmine::database_server}/${redmine::production_database}.*":
       user        => "${redmine::database_user}@${redmine::database_server}",
       privileges  => ['all'],
       table       => "${redmine::production_database}.*"
     }
 
-    mysql_grant { "${redmine::database_user}@${redmine::database_server}/${redmine::development_database}":
+    mysql_grant { "${redmine::database_user}@${redmine::database_server}/${redmine::development_database}.*":
       user        => "${redmine::database_user}@${redmine::database_server}",
       privileges => ['all'],
       table       => "${redmine::development_database}.*"
