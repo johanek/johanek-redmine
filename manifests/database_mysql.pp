4474a00c5da189edcf4f8ae1b097c387d4558b7a
Alexander Buddenbrock <a.buddenbrock@ish.de>
2014-12-10 20:45:46 +0100

    Fixed order in mysql database
    Set a fixed order of operations for mysql user and grants. Puppet might
    otherwise decide to create the user or grants before mysql::server did
    its job.

--- manifests/database_mysql.pp
+++ manifests/database_mysql.pp
@@ -7,17 +7,17 @@ class redmine::database_mysql {
     mysql_database { [$redmine::production_database,$redmine::development_database]:
       ensure  => present,
       charset => 'utf8'
-    }
+    } ->
 
     mysql_user { "${redmine::database_user}@${redmine::database_server}":
       password_hash => mysql_password($redmine::database_password)
-    }
+    } ->
 
     mysql_grant { "${redmine::database_user}@${redmine::database_server}/${redmine::production_database}.*":
       user       => "${redmine::database_user}@${redmine::database_server}",
       privileges => ['all'],
       table      => "${redmine::production_database}.*"
-    }
+    } ->
 
     mysql_grant { "${redmine::database_user}@${redmine::database_server}/${redmine::development_database}.*":
       user       => "${redmine::database_user}@${redmine::database_server}",
