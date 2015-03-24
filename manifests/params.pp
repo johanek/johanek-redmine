35e233839738ede25f62809f765823654856b624
Alexander Buddenbrock <a.buddenbrock@ish.de>
2015-03-24 20:46:29 +0100

    Added warning when using the default version
    See #38
    The default version will change with 3.0.0.
    As this is a breaking change that may suprise users, a warning will be
    printed if no version was set.

--- manifests/params.pp
+++ manifests/params.pp
@@ -24,12 +24,21 @@ class redmine::params {
       }
     }
   }
+
   if $redmine::database_adapter {
     $real_adapter = $redmine::database_adapter
   } elsif versioncmp($::rubyversion, '1.9') >= 0 {
     $real_adapter = 'mysql2'
   } else {
     $real_adapter = 'mysql'
+  }
+
+  if $redmine::version {
+    $version = $redmine::version
+  } else {
+    $version = '2.2.3'
+    warning('The default version will change to 2.6.4 in the next major release')
+    warning('If this is not what you want, please specify a version in hiera or your manifest')
   }
 
   case $redmine::provider {
